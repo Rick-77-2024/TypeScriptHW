6cc211a667283bbb097331bbdd244e94a1e94866
rick-77-2024 <rick_77@ukr.net>
2025-07-21 19:35:21 +0300

    Add files via upload

---
+++ hw3/hw3.2/js/main.ts
@@ -0,0 +1,6 @@
+// #TYj7ncx
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for (let x: number = 0; x <= 10; x++) {
+    document.write(`<div>${x}</div>`);
+}
---
+++ hw3/hw3.5/js/main.ts
@@ -0,0 +1,26 @@
+// #vLSZKMlO
+// - Використовуючи данні з масиву, за допомоги document.write та циклу побудувати структуру по шаблону
+// Масив:
+//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+const listOfItems: string[] = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+let x: number = 0;
+document.write('<ul>');
+while (x < listOfItems.length) {
+    document.write(`<li> ${listOfItems[x]} </li>`);
+    x++;
+}
+document.write('</ul>');

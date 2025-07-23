290537fba138f8dde8611db19184bc7029b38b17
rick-77-2024 <rick_77@ukr.net>
2025-07-23 13:52:18 +0300

    Delete hw6 directory

--- hw6/hw6.4/js/main.ts
+++
@@ -1,7 +0,0 @@
-// #0b89BkYZwu
-// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-
-let str: string = ' dirty string   '
-const s: string = str.trim();
-
-console.log(s);

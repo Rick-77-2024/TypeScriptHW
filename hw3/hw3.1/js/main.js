e80d9c392366c833d0f41d9c0ece0f088a777e0c
rick-77-2024 <rick_77@ukr.net>
2025-07-23 13:54:10 +0300

    Delete hw3 directory

--- hw3/hw3.1/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// #y7crMeFwHcS
-// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
-for (let x = 1; x <= 10; x++) {
-    document.write('<div>Div</div>' + x);
-}
--- hw3/hw3.2/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// #TYj7ncx
-// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
-for (let x = 0; x <= 10; x++) {
-    document.write(`<div>${x}</div>`);
-}

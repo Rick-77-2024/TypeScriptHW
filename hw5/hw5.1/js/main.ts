5a7814e1302abb4d8c7a427ba54b9158ac794d3c
rick-77-2024 <rick_77@ukr.net>
2025-07-23 13:52:11 +0300

    Delete hw5 directory

--- hw5/hw5.1/js/main.ts
+++
@@ -1,9 +0,0 @@
-// #I2XsG6f
-// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-
-const rectangle = (a: number, b: number): number => a * b;
-
-const sideA: number = 5;
-const sideB: number = 10;
-const area: number = rectangle(sideA, sideB);
-console.log(area);
--- hw5/hw5.2/js/main.ts
+++
@@ -1,6 +0,0 @@
-// #ETGAxbEn8l
-// - створити функцію яка обчислює та повертає площу кола з радіусом r
-
-const getCircleArea = (radius: number): number => Math.PI * radius * radius;
-
-console.log(getCircleArea(5));
